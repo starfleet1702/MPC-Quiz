b22a2203f2f126e17905bdb9b9444dec5c3cbcd6
Nishant Kamleshbhai Katariya <Nishant.Katariya@ad.infosys.com>
2018-04-26 18:55:58 +0530

    Initial commit

---
+++ global_kinematic_model/src/main.cpp
@@ -0,0 +1,47 @@
+// In this quiz you'll implement the global kinematic model.
+#include <math.h>
+#include <iostream>
+#include "Eigen-3.3/Eigen/Core"
+
+//
+// Helper functions
+//
+constexpr double pi() { return M_PI; }
+double deg2rad(double x) { return x * pi() / 180; }
+double rad2deg(double x) { return x * 180 / pi(); }
+
+const double Lf = 2;
+
+// TODO: Implement the global kinematic model.
+// Return the next state.
+//
+// NOTE: state is [x, y, psi, v]
+// NOTE: actuators is [delta, a]
+Eigen::VectorXd globalKinematic(Eigen::VectorXd state,
+                                Eigen::VectorXd actuators, double dt) {
+  Eigen::VectorXd next_state(state.size());
+  
+  double new_x = state[0] + state[3]*cos(state[2])*dt; // x = x0 + v*cos(psi)*dt
+  double new_y = state[1] + state[3]*sin(state[2])*dt; // y = y0 + v*sin(psi)*dt
+  double new_psi = state[2] + (state[3]/Lf)*actuators[0]*dt; // psi = psi0 + (v/Lf)*delta*dt;
+  double new_v = state[3] + actuators[1]*dt; // v = v0 + a*dt;
+  
+  next_state << new_x,new_y,new_psi,new_v;
+  
+  return next_state;
+}
+
+int main() {
+  // [x, y, psi, v]
+  Eigen::VectorXd state(4);
+  // [delta, v]
+  Eigen::VectorXd actuators(2);
+
+  state << 0, 0, deg2rad(45), 1;
+  actuators << deg2rad(5), 1;
+
+  // should be [0.212132, 0.212132, 0.798488, 1.3]
+  auto next_state = globalKinematic(state, actuators, 0.3);
+
+  std::cout << next_state << std::endl;
+}
